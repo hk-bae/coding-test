45f855e85b206789e651c437af7e72c3f69e2cf2
hk-bae <68215452+hk-bae@users.noreply.github.com>
2021-08-17 16:55:20 +0900

    Add boj problems

---
+++ baekjoon/implementation_19237.py
@@ -0,0 +1,114 @@
+import sys
+
+input = sys.stdin.readline
+
+n,m,k = map(int,input().split())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+# 상어 냄새 정보 (상어번호, 사라지는 시간)
+area = [[[0,0] for _ in range(n)] for _ in range(n)]
+
+# 상어 위치 정보
+location = [[] for _ in range(m+1)]
+
+# 1. 자신의 위치에 냄새 뿌리기
+for i in range(n) :
+    for j in range(n) :
+        if graph[i][j] != 0 :
+            area[i][j] = [graph[i][j],k]
+            location[graph[i][j]] = [i,j]
+
+# 1,2,3,4 : 상,하,좌,우
+dx = [0,-1,1,0,0]
+dy = [0,0,0,-1,1]
+
+# 각 상어의 현재 방향
+direction = [0] + list(map(int,input().split()))
+
+# 각 상어의 방향 우선순위
+priority = [[[] for _ in range(5)] for _ in range(m+1)]
+
+for i in range(1,m+1) :
+    for j in range(1,5) :
+        priority[i][j] = [0] + list(map(int,input().split()))
+
+# for i in range(0,m+1) :
+#     for j in range(0,5) :
+#         print(priority[i][j],end = ' ')
+#     print()
+
+time = 0
+alive = [True] * (m+1) # 생존 목록
+alive_cnt = m # 살아있는 상어의 수
+
+
+while True :
+
+    time += 1 
+
+    empty_area = [[False] * n for _ in range(n)] # 빈공간 체크
+    for i in range(n) :
+        for j in range(n) :
+            if area[i][j] == [0,0] :
+                empty_area[i][j] = True
+
+    # 상어 이동
+    # 모든 상어에 대해서 탐색
+    for i in range(1,m+1) : 
+        if alive[i] : # i번째 상어가 살아있는 경우
+            x,y = location[i] # 현재 상어의 위치
+            now_d = direction[i] # 현재 상어가 바라보는 방향
+            
+            move = False
+            
+            # 우선순위 순으로 해당 위치가 비어있는지 탐색
+            for j in range(1,5) :
+
+                nx = x + dx[priority[i][now_d][j]]
+                ny = y + dy[priority[i][now_d][j]]
+                if 0<=nx<n and 0<=ny<n and empty_area[nx][ny]: # 그 위치가 비어있는 공간인 경우
+                    if area[nx][ny] == [0,0] :
+                        # 상어를 이동
+                        area[nx][ny] = [i,time + k] 
+                        direction[i] = priority[i][now_d][j] # 바라보는 방향 갱신
+                        location[i] = [nx,ny] # 상어 위치 갱신
+                        move = True
+                        break
+                    else : # 더 낮은 번호의 상어가 이미 선점한 경우
+                        print("#",i,nx,ny,area[nx][ny],time)
+                        alive_cnt -= 1
+                        alive[i] = False
+                        break
+
+            
+            # 비어있는 공간이 없다면 자신의 냄새가 있던 공간을 탐색
+            if alive[i] and not move : 
+                for j in range(1,5) :
+                    nx = x + dx[priority[i][now_d][j]]
+                    ny = y + dy[priority[i][now_d][j]]
+                    if 0<=nx<n and 0<=ny<n and area[nx][ny][0] == i :  # 자신의 냄새가 존재하는 경우
+                        # 상어를 이동
+                        area[nx][ny][1] = time + k
+                        direction[i] = priority[i][now_d][j]
+                        location[i] = [nx,ny] # 상어 위치 갱신
+                        break
+
+    # 1마리만 살아 남으면 종료
+    if alive_cnt == 1 :
+        break
+
+    # 1000초가 되면 -1
+    if time == 1000 :
+        time = -1
+        break
+
+    # 상어 냄새 감소
+    for i in range(n) :
+        for j in range(n) :
+            if area[i][j][1] == time : 
+                area[i][j] = [0,0] # 냄새 제거
+
+print(time)
+
+
---
+++ baekjoon/implementation_21609.py
@@ -0,0 +1,153 @@
+import sys
+from collections import deque
+
+n,m = map(int,input().split())
+
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+score = 0 
+
+# 블록 그룹이 존재할때 까지 반복
+while True : 
+    visited = [[False] * n for _ in range(n)]
+    max_size,max_rainbow = 0,0 # 최대 크기, 최대크기일때 무지개 블럭 개수
+    standard_x,standard_y = 0,0 # 기준 블록
+    block_group = [] # (x,y)
+
+    # 1. 크기가 가장 큰 블록 그룹 찾기
+    for i in range(n) :
+        for j in range(n) :
+            # 아직 그룹이 형성되지 않은 블록 중 일반 블록에서 부터 시작
+            if not visited[i][j] and graph[i][j] >= 1 and graph[i][j] <= m :
+                visited[i][j] = True
+                q = deque([(i,j)])
+                tmp_size,tmp_rainbow = 1,0
+                tmp_group = [(i,j)]
+                rainbow_group = []
+                value = graph[i][j] # 찾아야할 색상
+                tmp_x,tmp_y = i,j # 현재 그룹의 기준 블럭
+
+                while q :
+                    now_x,now_y = q.popleft()
+                    for d in range(4) :
+                        nx = now_x + dx[d]
+                        ny = now_y + dy[d]
+                        #  인접한 같은 색상 또는 무지개 블럭을 추가
+                        if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and (graph[nx][ny] == 0 or graph[nx][ny] == value):
+                            visited[nx][ny] = True
+                            q.append((nx,ny))
+                            tmp_group.append((nx,ny)) # 현재 블록 그룹 저장
+                            tmp_size += 1 
+                            if graph[nx][ny] == 0 : # 무지개블록인지 확인
+                                tmp_rainbow += 1
+                                rainbow_group.append((nx,ny))
+
+                            # 기준 블록이 변화하는지 확인.
+                            if tmp_x > nx or (tmp_x == nx and tmp_y > ny) :
+                                tmp_x = nx
+                                tmp_y = ny    
+                                
+                                    
+
+
+                # 무지개 블록은 다른 블록그룹에 포함될 수 있음으로 방문 False 처리
+                for x,y in rainbow_group :
+                    visited[x][y] = False
+
+                # 2보다 작은 크기는 블록이 아직 형성되지 않은 것
+                if tmp_size < 2 :
+                    continue
+                
+                # 가장 큰 블록 그룹과 비교
+                if tmp_size > max_size : # 크기 우선 비교
+                    max_size,max_rainbow,block_group,standard_x,standard_y = tmp_size,tmp_rainbow,tmp_group,tmp_x,tmp_y
+                    print(tmp_size,i,j)
+                elif tmp_size == max_size : # 크기가 같을 경우
+                    if tmp_rainbow > max_rainbow : # 무지개 블록 개수 비교
+                        max_size,max_rainbow,block_group,standard_x,standard_y = tmp_size,tmp_rainbow,tmp_group,tmp_x,tmp_y
+                        print(tmp_size,i,j)
+                    elif tmp_rainbow == max_rainbow : # 무지개 블록 개수도 같을 경우
+                        if standard_x < tmp_x or (standard_x == tmp_x and standard_y < tmp_y) : # 행,열 비교
+                            max_size,max_rainbow,block_group,standard_x,standard_y = tmp_size,tmp_rainbow,tmp_group,tmp_x,tmp_y
+                            print(tmp_size,i,j)
+                         
+            
+    # 더이상 블록이 남아있지 않는 경우
+    if max_size == 0 : 
+        break
+
+    print(max_size)
+
+    # 2. 1에서 찾은 블록 제거
+    for x,y in block_group :
+        graph[x][y] = -2
+
+    score += pow(max_size,2)
+    print("###maxsize;",max_size)
+
+    print("after remove")
+    for i in range(n) :
+        for j in range(n) :
+            print(graph[i][j],end = ' ' )
+        print()
+
+
+    
+    #3. 격자 중력 작용
+    after_gravity = [[-2] * n for _ in range(n)]
+    for j in range(n) :
+        next = n-1
+        for i in range(n-1,-1,-1) :
+            if graph[i][j] != -1 and graph[i][j] != -2 : # 일반 블록, 무지개 블록인 경우
+                after_gravity[next][j] = graph[i][j]
+                next -= 1
+            elif graph[i][j] == -1 : # 검정블록인 경우
+                next = i - 1
+                after_gravity[i][j] = -1
+
+    print("after gravity")
+    for i in range(n) :
+        for j in range(n) :
+            print(after_gravity[i][j],end = ' ' )
+        print()
+
+    #4. 90 도 반시계 회전
+    after_rotate = [[-2] * n for _ in range(n)]
+    for i in range(n) :
+        for j in range(n) :
+            after_rotate[n-j-1][i] = after_gravity[i][j]
+
+    print("after rotate")
+    for i in range(n) :
+        for j in range(n) :
+            print(after_rotate[i][j],end = ' ' )
+        print()
+
+    #5. 격자 중력 작용
+    after_gravity = [[-2] * n for _ in range(n)]
+    for j in range(n) :
+        next = n-1
+        for i in range(n-1,-1,-1) :
+            if after_rotate[i][j] != -1 and after_rotate[i][j] != -2 : # 일반 블록, 무지개 블록인 경우
+                after_gravity[next][j] = after_rotate[i][j]
+                next -= 1
+            elif after_rotate[i][j] == -1 : # 검정블록인 경우
+                next = i - 1
+                after_gravity[i][j] = -1
+    
+    # 다시 그래프로 복사
+    for i in range(n) :
+        for j in range(n) :
+            graph[i][j] = after_gravity[i][j]
+
+    print("after gravity2")
+    for i in range(n) :
+        for j in range(n) :
+            print(graph[i][j],end = ' ' )
+        print()
+
+
+print(score)
